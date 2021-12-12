30911b623a9291fec6d068652cf0bb48db874994
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2021-12-12 13:20:13 +0700

    fixed &apos;

--- pages/docs/index.tsx
+++ pages/docs/index.tsx
@@ -1,6 +1,7 @@
 import type { NextPage } from 'next'
 import { useRouter } from 'next/router'
 import React from 'react'
+import { Element } from '@nodestrap/element'
 
 
 
@@ -9,7 +10,7 @@ const Download: NextPage = () => {
     router.replace('/docs/getting-started')
     
     return (
-        <p>Redirecting to <a href='/docs/getting-started'>/docs/getting-started</a>...</p>
+        <p>Redirecting to <Element tag='a' {...{href:'/docs/getting-started'}}>/docs/getting-started</Element>...</p>
     )
 }
 
