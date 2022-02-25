ee576b70bc82173fa19006e8a76265a99b4d625c
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2022-02-25 21:29:21 +0700

    added common@List

--- pages/docs/components/list.tsx
+++ pages/docs/components/list.tsx
@@ -8,49 +8,47 @@ import { useFlipFlop } from '../../../components/hooks'
 import { SpecList, SubSpecList, DetailSpecItem, SimpleSpecItem } from '../../../components/SpecList'
 
 import { TransparentPreview } from '../../../components/TransparentPreview'
-import { SectionInheritedProps, LinkListPage, LinkIndicatorPage, SectionOverridingDefaults, SectionCustomizingCss, ComponentInfoProvider, SectionDerivering, SectionCustomizing, SectionIntro, SectionDemo, BusyBar, CurrentComponent, CurrentNestedComponent, CurrentBaseComponents, LinkListItemPage, SectionVariants, SectionStates, LinkContentPage, LinkButtonPage, LinkListSeparatorItemPage, LinkUsesIndicatorVariantsPage, useComponentInfo, SectionPreviewProperty, SectionPreviewPropertyProps } from '../../../components/common'
+import { SectionInheritedProps, LinkListPage, LinkIndicatorPage, SectionOverridingDefaults, SectionCustomizingCss, ComponentInfoProvider, SectionDerivering, SectionCustomizing, SectionIntro, SectionDemo, BusyBar, CurrentComponent, CurrentBaseComponents, LinkListItemPage, SectionVariants, SectionStates, LinkListSeparatorItemPage, LinkUsesIndicatorVariantsPage, SectionPreviewProperty } from '../../../components/common'
 import { List, ListItem, ListSeparatorItem, OrientationName } from '@nodestrap/list'
 import { TypeScriptCode } from '../../../components/Code'
 import ResponsiveProvider from '@nodestrap/responsive'
-import { Tips, Warning } from '../../../components/Info'
+import { Tips } from '../../../components/Info'
 import Element from '@nodestrap/element'
 import {
     SectionPropertyTheme,
     SectionPropertySize,
-    SectionPropertyNudeProps,
-    SectionPropertyNude     as BasicSectionPropertyNude,
     SectionPropertyGradient,
     SectionPropertyOutlined,
-    SectionPropertyMildProps,
-    SectionPropertyMild     as BasicSectionPropertyMild,
     SectionPropertyOrientation,
     SectionPropertyOrientationBlock,
     SectionPropertyOrientationInline,
     SectionPropertyOrientationResponsive,
-    SectionPropertyStyleProps,
-    SectionPropertyStyle,
-    SectionPropertyItemStyleProps,
-    SectionPropertyItemStyle,
-    SectionPropertyFlatStyle   as BasicSectionPropertyFlatStyle,
-    SectionPropertyFlushStyle  as BasicSectionPropertyFlushStyle,
-    SectionPropertyJoinedStyle as BasicSectionPropertyJoinedStyle,
 } from '../../../components/common@Basic'
+import {
+    SectionPropertyNude,
+    SectionPropertyMild,
+    SectionPropertyListStyle,
+    SectionPropertyFlatStyle,
+    SectionPropertyFlushStyle,
+    SectionPropertyJoinedStyle,
+    SectionPropertyContentStyle,
+    SectionPropertyBtnStyle,
+    SectionPropertyTabStyle,
+    SectionPropertyBreadcrumbStyle,
+    SectionPropertyBulletStyle,
+    SectionPropertyNumberedStyle,
+    ContentMakeListItemClickable,
+    SectionPropertyActionCtrl,
+    SectionPropertyActive,
+    SectionPropertyActiveNoOutlined,
+    SectionPropertyEnabled,
+} from '../../../components/common@List'
 
 import loadable from '@loadable/component'
 const DemoListLazy = loadable(() => import(/* webpackChunkName: 'DemoPanel@List' */'../../../components/DemoPanel@List'))
 
 
 
-export const SectionPropertyNude = ({ noBorder = true, ...props }: SectionPropertyNudeProps) => {
-    return (
-        <BasicSectionPropertyNude {...props} noBorder={noBorder} />
-    );
-};
-export const SectionPropertyMild = ({ setByDefault = true, ...props }: SectionPropertyMildProps) => {
-    return (
-        <BasicSectionPropertyMild {...props} setByDefault={setByDefault} />
-    );
-};
 const FunctionalTabList = () => {
     const [activeTabIndex, setActiveTabIndex] = useState(3);
     
@@ -75,168 +73,6 @@ const FunctionalTabList = () => {
             </ListItem>
         </List>
     );
-};
-const listStyle = 'listStyle';
-export const SectionPropertyListStyle       = ({ styleName = listStyle, ...restProps }: SectionPropertyStyleProps) => {
-    return (
-        <SectionPropertyStyle {...restProps} styleName={styleName} />
-    );
-};
-export const SectionPropertyFlatStyle       = ({ styleName = listStyle, ...restProps }: SectionPropertyItemStyleProps) => {
-    return (
-        <BasicSectionPropertyFlatStyle {...restProps} styleName={styleName} />
-    );
-};
-export const SectionPropertyFlushStyle      = ({ styleName = listStyle, ...restProps }: SectionPropertyItemStyleProps) => {
-    return (
-        <BasicSectionPropertyFlushStyle {...restProps} styleName={styleName} />
-    );
-};
-export const SectionPropertyJoinedStyle     = ({ styleName = listStyle, ...restProps }: SectionPropertyItemStyleProps) => {
-    return (
-        <BasicSectionPropertyJoinedStyle {...restProps} styleName={styleName} />
-    );
-};
-export const SectionPropertyContentStyle    = ({ styleName = listStyle, property = 'content'   , description, ...restProps }: SectionPropertyItemStyleProps) => {
-    const { componentName } = useComponentInfo();
-    
-    
-    
-    return (
-        <SectionPropertyItemStyle {...restProps} styleName={styleName} property={property} description={
-            description
-            ??
-            <>
-                <p>
-                    Set <code>{`<${componentName} ${styleName}='${property}'>`}</code> to style the <CurrentNestedComponent /> to look similar to <LinkContentPage /> (usually with wider paddings).
-                </p>
-                <p>
-                    Basically it merges the <CurrentNestedComponent />&apos;s style + <LinkContentPage />&apos;s style.
-                </p>
-            </>
-        } />
-    );
-};
-export const SectionPropertyBtnStyle        = ({ styleName = listStyle, property = 'btn'       , description, ...restProps }: SectionPropertyItemStyleProps) => {
-    const { componentName } = useComponentInfo();
-    
-    
-    
-    return (
-        <SectionPropertyItemStyle {...restProps} styleName={styleName} property={property} description={
-            description
-            ??
-            <>
-                <p>
-                    Set <code>{`<${componentName} ${styleName}='${property}'>`}</code> to style the <CurrentNestedComponent /> to look similar to <LinkButtonPage />.
-                </p>
-                <p>
-                    Basically it merges the <CurrentNestedComponent />&apos;s style + <LinkButtonPage />&apos;s style.
-                </p>
-            </>
-        } />
-    );
-};
-export const SectionPropertyTabStyle        = ({ styleName = listStyle, property = 'tab'       , description, ...restProps }: SectionPropertyItemStyleProps) => {
-    const { componentName } = useComponentInfo();
-    
-    
-    
-    return (
-        <SectionPropertyItemStyle {...restProps} styleName={styleName} property={property} description={
-            description
-            ??
-            <>
-                <p>
-                    Set <code>{`<${componentName} ${styleName}='${property}'>`}</code> to style the <CurrentNestedComponent /> to look similar to <em>a tabbed interface</em>.
-                </p>
-            </>
-        } />
-    );
-};
-export const SectionPropertyBreadcrumbStyle = ({ styleName = listStyle, property = 'breadcrumb', description, ...restProps }: SectionPropertyItemStyleProps) => {
-    const { componentName } = useComponentInfo();
-    
-    
-    
-    return (
-        <SectionPropertyItemStyle {...restProps} styleName={styleName} property={property} description={
-            description
-            ??
-            <>
-                <p>
-                    Set <code>{`<${componentName} ${styleName}='${property}'>`}</code> to style the <CurrentNestedComponent /> to look similar to <em>a breadcrumb</em>.
-                </p>
-                <p>
-                    Requires <code>{`<${componentName} orientation='inline'>`}</code>.
-                </p>
-            </>
-        } />
-    );
-};
-export const SectionPropertyBulletStyle     = ({ styleName = listStyle, property = 'bullet'    , description, ...restProps }: SectionPropertyItemStyleProps) => {
-    const { componentName } = useComponentInfo();
-    
-    
-    
-    return (
-        <SectionPropertyItemStyle {...restProps} styleName={styleName} property={property} description={
-            description
-            ??
-            <>
-                <p>
-                    Set <code>{`<${componentName} ${styleName}='${property}'>`}</code> to style the <CurrentNestedComponent /> to look similar to <em>a bullet</em>.
-                </p>
-                <p>
-                    Requires an <strong>empty</strong> children of <CurrentNestedComponent />.
-                </p>
-            </>
-        } />
-    );
-};
-export const SectionPropertyNumberedStyle   = ({ styleName = listStyle, property = 'numbered'  , description, ...restProps }: SectionPropertyItemStyleProps) => {
-    const { componentName } = useComponentInfo();
-    
-    
-    
-    return (
-        <SectionPropertyItemStyle {...restProps} styleName={styleName} property={property} description={
-            description
-            ??
-            <>
-                <p>
-                    Set <code>{`<${componentName} ${styleName}='${property}'>`}</code> to add a number (a counter) on each <CurrentNestedComponent />.
-                </p>
-            </>
-        } />
-    );
-};
-
-const ContentMakeListItemClickable = () => {
-    return (
-        <>
-            <p>
-                To make <LinkListItemPage /> clickable, set <code>{`<ListItem actionCtrl={true}>`}</code>.
-            </p>
-            <p>
-                You can also set the <code>actionCtrl</code> at <code>{`<List actionCtrl={true}>`}</code>, so the default value of <code>actionCtrl</code> in the <LinkListItemPage /> will be the same as the parent.
-            </p>
-            <p>
-                To handle the click action of the <LinkListItemPage />, assign <code>onClick</code> to the desired <strong>handler function</strong> -or- assign <code>href</code> to the desired <strong>URL</strong>.
-            </p>
-            <Warning>
-                <p>
-                    Assigning <code>{`actionCtrl={true}`}</code> and <code>href</code> makes the <LinkListItemPage /> rendered as <code>{`<a>`}</code>.
-                </p>
-                <p>
-                    Assigning <code>{`actionCtrl={true}`}</code> without assigning <code>href</code> makes the <LinkListItemPage /> rendered as <code>{`<div role='button'>`}</code>.
-                </p>
-                <p>
-                    Assigning <code>{`actionCtrl={true}`}</code> and <code>{`tag='button'`}</code> makes the <LinkListItemPage /> rendered as <code>{`<button>`}</code>.
-                </p>
-            </Warning>
-        </>
-    )
 };
 const ListWithActiveOutlined = () => {
     const [listRef, isActive] = useFlipFlop({ defaultState: true });
@@ -268,66 +104,6 @@ const ListWithActiveOutlined = () => {
                 </p>
             </ListItem>
         </List>
-    );
-};
-export const SectionPropertyActionCtrl       = ({ property = 'actionCtrl', properties = 'Links and Buttons'                    , description, ...restProps }: SectionPreviewPropertyProps) => {
-    return (
-        <SectionPreviewProperty {...restProps} property={property} properties={properties} description={
-            description
-            ??
-            <>
-                <ContentMakeListItemClickable />
-            </>
-        } />
-    );
-};
-export const SectionPropertyActive           = ({ property = 'active'    , properties = 'Active Items'                         , description, ...restProps }: SectionPreviewPropertyProps) => {
-    return (
-        <SectionPreviewProperty {...restProps} property={property} properties={properties} description={
-            description
-            ??
-            <>
-                <p>
-                    To make <LinkListItemPage /> active, set <code>{`<ListItem active={true}>`}</code>.
-                </p>
-                <p>
-                    You can also set the <code>active</code> at <code>{`<List active={true}>`}</code>, so the entire <LinkListItemPage />s are active.<br />
-                    To make an exception in a/some <LinkListItemPage />(s), set <code>{`<ListItem active={false} inheritActive={false}>`}</code>.<br />
-                    Note: the <code>{`inheritActive={false}`}</code> prevents the active state on <CurrentComponent /> affecting the <LinkListItemPage />.
-                </p>
-            </>
-        } />
-    );
-};
-export const SectionPropertyActiveNoOutlined = ({ property = 'outlined'  , properties = 'Active State cancels Outlined Variant', description, ...restProps }: SectionPreviewPropertyProps) => {
-    return (
-        <SectionPreviewProperty {...restProps} titleTag={'h4'} property={property} properties={properties} description={
-            description
-            ??
-            <>
-                <p>
-                    Assigning <code>{`<ListItem active={true}>`}</code> will cancel out the <code>outlined</code> variant.
-                </p>
-            </>
-        } />
-    );
-};
-export const SectionPropertyEnabled          = ({ property = 'enabled'   , properties = 'Disabled Items'                       , description, ...restProps }: SectionPreviewPropertyProps) => {
-    return (
-        <SectionPreviewProperty {...restProps} property={property} properties={properties} description={
-            description
-            ??
-            <>
-                <p>
-                    To make <LinkListItemPage /> disabled, set <code>{`<ListItem enabled={false}>`}</code>.
-                </p>
-                <p>
-                    You can also set the <code>enabled</code> at <code>{`<List enabled={false}>`}</code>, so the entire <LinkListItemPage />s are disabled.<br />
-                    To make an exception in a/some <LinkListItemPage />(s), set <code>{`<ListItem enabled={true} inheritEnabled={false}>`}</code>.<br />
-                    Note: the <code>{`inheritEnabled={false}`}</code> prevents the disabled state on <CurrentComponent /> affecting the <LinkListItemPage />.
-                </p>
-            </>
-        } />
     );
 };
 
