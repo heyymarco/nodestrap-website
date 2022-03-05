309ce95ae1f1056f97a7c7e0d17d905fcf517ae7
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2022-03-05 23:49:29 +0700

    updated badge

--- pages/docs/components/badge.tsx
+++ pages/docs/components/badge.tsx
@@ -413,7 +413,7 @@ const Page: NextPage = () => {
                     during <em>showing</em>/<em>hiding</em> transition, the <CurrentComponent /> <strong>shifts</strong> the position of <em>next siblings</em>.
                 </p>
                 <p>
-                    The preview below illustrates the <CurrentComponent /> <strong>shifts</strong> the position of <strong>a logout button</strong>:
+                    The preview below illustrates the <CurrentComponent /> <strong>shifts</strong> the position of a <strong>logout button</strong>:
                 </p>
                 <OverlayBadgePreview overlay={false} />
                 <p>
