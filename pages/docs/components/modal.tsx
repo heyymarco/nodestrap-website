d3da0d61df484215890fa5c26220cfe2bc9e8bbb
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2022-03-29 22:04:03 +0700

    updated modal

--- pages/docs/components/modal.tsx
+++ pages/docs/components/modal.tsx
@@ -55,7 +55,7 @@ const DemoModalLazy = loadable(() => import(/* webpackChunkName: 'DemoPanel@Moda
 const Modal = (props: ModalProps) => {
     const viewportRef = useRef(null);
     
-    return (<div ref={viewportRef} style={{ position: 'relative', padding: '1rem' }} className='media'>
+    return (<div ref={viewportRef} style={{ position: 'relative', padding: '1rem', height: '15rem', overflow: 'hidden' }} className='media'>
         <ModalOri
             {...props}
             theme={props.theme ?? 'primary'}
@@ -116,10 +116,10 @@ const Page: NextPage = () => {
 
             <SectionIntro>
                 <p>
-                    Dynamically <strong>shows</strong> and <strong>hides</strong> a content by sliding animation either vertically or horizontally, depending on the orientation.
+                    Turns any component to <strong>modal dialog</strong> component.
                 </p>
                 <p>
-                    Similar to <CurrentBaseComponents /> but <em>aware</em> of its orientation for the sliding direction.
+                    Similar to native <code>{`<dialog>`}</code> but this is the <em>polyfill</em> version with more rich features and fully controllable component.
                 </p>
                 <p>
                     Here the preview:
@@ -153,9 +153,9 @@ const Page: NextPage = () => {
     theme='${themeName}'
     active={true}
 >
-    <p>
+    <Content>
         A <code>{'<Modal>'}</code> with ${themeName} theme.
-    </p>
+    </Content>
 </Modal>
 `
                         ).join('')}
@@ -198,9 +198,9 @@ const Page: NextPage = () => {
     active={true}
     theme='primary'
 >
-    <p>
+    <Content>
         A <code>{'<Modal>'}</code> with smaller size.
-    </p>
+    </Content>
 </Modal>
 
 <Modal
@@ -208,9 +208,9 @@ const Page: NextPage = () => {
     active={true}
     theme='primary'
 >
-    <p>
+    <Content>
         A <code>{'<Modal>'}</code> with default size.
-    </p>
+    </Content>
 </Modal>
 
 <Modal
@@ -218,9 +218,9 @@ const Page: NextPage = () => {
     active={true}
     theme='primary'
 >
-    <p>
+    <Content>
         A <code>{'<Modal>'}</code> with larger size.
-    </p>
+    </Content>
 </Modal>
                     `}</TypeScriptCode>
                 </SectionPropertySize>
@@ -243,9 +243,9 @@ const Page: NextPage = () => {
     active={true}
     theme='primary'
 >
-    <p>
+    <Content>
         A nude <code>{'<Modal>'}</code> 😋
-    </p>
+    </Content>
 </Modal>
                     `}</TypeScriptCode>
                 </SectionPropertyNude>
@@ -273,9 +273,9 @@ const Page: NextPage = () => {
     active={true}
     theme='${themeName}'
 >
-    <p>
+    <Content>
         Hello world!
-    </p>
+    </Content>
 </Modal>
 `
                         ).join('')}
@@ -305,9 +305,9 @@ const Page: NextPage = () => {
     active={true}
     theme='${themeName}'
 >
-    <p>
+    <Content>
         Hello world!
-    </p>
+    </Content>
 </Modal>
 `
                         ).join('')}
@@ -337,9 +337,9 @@ const Page: NextPage = () => {
     active={true}
     theme='${themeName}'
 >
-    <p>
+    <Content>
         Hello world!
-    </p>
+    </Content>
 </Modal>
 `
                         ).join('')}
@@ -364,9 +364,9 @@ const Page: NextPage = () => {
     active={true}
     theme='primary'
 >
-    <p>
+    <Content>
         Hello world!
-    </p>
+    </Content>
 </Modal>
                     `}</TypeScriptCode>
                 </SectionPropertyActive>
@@ -389,9 +389,9 @@ const Page: NextPage = () => {
     active={true}
     theme='primary'
 >
-    <p>
+    <Content>
         Hello world!
-    </p>
+    </Content>
 </Modal>
                     `}</TypeScriptCode>
                 </SectionPropertyEnabled>
@@ -399,13 +399,20 @@ const Page: NextPage = () => {
             <SectionPropertyLazy />
             <SectionVariables specList={
                 <SpecList>
-                    <DetailSpecItem title='Animations'>
+                    <DetailSpecItem title='Foregrounds, Backgrounds &amp; Borders'>
                         <SubSpecList>
                             <SimpleSpecItem>
-                                <code>filterActive</code>
-                                <p>A <code>filter</code> to apply when <code>{`active={true}`}</code>.</p>
+                                <code>backg</code>
+                                <p>A <code>background</code> to apply to the backdrop, usually a dark color, so the content behind the dialog appears dimmed.</p>
                             </SimpleSpecItem>
-                            
+                            <SimpleSpecItem>
+                                <code>componentBoxShadow</code>
+                                <p>A <code>boxShadow</code> to apply to the dialog, so the dialog appears hovered.</p>
+                            </SimpleSpecItem>
+                        </SubSpecList>
+                    </DetailSpecItem>
+                    <DetailSpecItem title='Animations'>
+                        <SubSpecList>
                             <SimpleSpecItem>
                                 <code>@keyframes active</code>
                                 <p>A default keyframes name represents <em>showing keyframes</em>, transition from <code>{`active={false}`}</code> to <code>{`active={true}`}</code>.</p>
@@ -422,23 +429,6 @@ const Page: NextPage = () => {
                                 <code>animPassive</code>
                                 <p>A default animation represents <em>hiding animation</em>, transition from <code>{`active={true}`}</code> to <code>{`active={false}`}</code>.</p>
                             </SimpleSpecItem>
-                            
-                            <SimpleSpecItem>
-                                <code>@keyframes activeInline</code>
-                                <p>A keyframes name represents <em>showing keyframes</em> when <code>{`orientation='inline'`}</code>, transition from <code>{`active={false}`}</code> to <code>{`active={true}`}</code>.</p>
-                            </SimpleSpecItem>
-                            <SimpleSpecItem>
-                                <code>@keyframes passiveInline</code>
-                                <p>A keyframes name represents <em>hiding keyframes</em> when <code>{`orientation='inline'`}</code>, transition from <code>{`active={true}`}</code> to <code>{`active={false}`}</code>.</p>
-                            </SimpleSpecItem>
-                            <SimpleSpecItem>
-                                <code>animActiveInline</code>
-                                <p>An animation represents <em>showing animation</em> when <code>{`orientation='inline'`}</code>, transition from <code>{`active={false}`}</code> to <code>{`active={true}`}</code>.</p>
-                            </SimpleSpecItem>
-                            <SimpleSpecItem>
-                                <code>animPassiveInline</code>
-                                <p>An animation represents <em>hiding animation</em> when <code>{`orientation='inline'`}</code>, transition from <code>{`active={true}`}</code> to <code>{`active={false}`}</code>.</p>
-                            </SimpleSpecItem>
                         </SubSpecList>
                     </DetailSpecItem>
                 </SpecList>
@@ -447,7 +437,7 @@ const Page: NextPage = () => {
                 <SectionOverridingDefaults>{`
 import { Modal } from '@nodestrap/modal'
 
-export default function PopupLoginForm(props) {
+export default function LoginDialog(props) {
     return (
         <Modal
             {...props} // preserves other properties
@@ -458,7 +448,22 @@ export default function PopupLoginForm(props) {
             theme={props.theme ?? 'primary'} // override default value of theme to 'primary'
             mild={props.mild ?? false}       // override default value of mild  to false
         >
-            { props.children }
+            <div role='dialog'>
+                <TextInput  placeholder='John Smith'     size='sm' />
+                <EmailInput placeholder='john@smith.com' size='sm' />
+                <Button
+                    theme='primary'
+                    size='sm'
+                >
+                    Submit
+                </Button>
+                <Button
+                    theme='secondary'
+                    size='sm'
+                >
+                    Cancel
+                </Button>
+            </div>
         </Modal>
     );
 }
@@ -466,43 +471,61 @@ export default function PopupLoginForm(props) {
 
                 <SectionCustomizingCss specList={
                     <SpecList>
-                        <DetailSpecItem code='usesModalLayout()'>
+                        <DetailSpecItem code='usesDialogLayout()'>
                             <p>
-                                Returns a <code>Rule</code> object represents a complete <CurrentComponent /> <strong>layout</strong> except its <strong>variants</strong> and <strong>states</strong>.
+                                Returns a <code>Rule</code> object represents a complete <CurrentComponent />&apos;s dialog <strong>layout</strong> except its <strong>states</strong>.
                             </p>
                         </DetailSpecItem>
-                        <DetailSpecItem code='usesModalVariants()'>
+                        <DetailSpecItem code='usesDialogStates()'>
                             <p>
-                                Returns a <code>Rule</code> object represents the <strong>variants</strong> of <CurrentComponent /> such as:<br />
-                                <code>SizeVariant</code>, <code>OrientationVariant</code>, and <strong>all variants</strong> inherited from <CurrentBaseComponents />.
+                                Returns a <code>Rule</code> object represents the <strong>states</strong> of <CurrentComponent />&apos;s dialog such as:<br />
+                                <strong>excited</strong>.
                             </p>
                         </DetailSpecItem>
-                        <DetailSpecItem code='usesModalStates()'>
+                        
+                        <DetailSpecItem code='usesBackdropLayout()'>
                             <p>
-                                Returns a <code>Rule</code> object represents the <strong>states</strong> of <CurrentComponent /> such as:<br />
-                                <strong>active</strong>/<strong>passive</strong> and <strong>all states</strong> inherited from <CurrentBaseComponents />.
+                                Returns a <code>Rule</code> object represents a complete <CurrentComponent />&apos;s backdrop <strong>layout</strong> except its <strong>variants</strong> and <strong>states</strong>.
+                            </p>
+                        </DetailSpecItem>
+                        <DetailSpecItem code='usesBackdropVariants()'>
+                            <p>
+                                Returns a <code>Rule</code> object represents the <strong>variants</strong> of <CurrentComponent />&apos;s backdrop such as:<br />
+                                <code>SizeVariant</code> and <code>BackdropVariant</code>.
+                            </p>
+                        </DetailSpecItem>
+                        <DetailSpecItem code='usesBackdropStates()'>
+                            <p>
+                                Returns a <code>Rule</code> object represents the <strong>states</strong> of <CurrentComponent />&apos;s backdrop such as:<br />
+                                <strong>active</strong> and <strong>passive</strong>.
                             </p>
                         </DetailSpecItem>
                     </SpecList>
                 }>{`
-import { mainComposition, style, imports, variants, states, rule } from '@cssfn/cssfn'
+import { mainComposition, style, imports, variants, rule, children } from '@cssfn/cssfn'
 import { createUseSheet } from '@cssfn/react-cssfn'
-import { isActive } from '@nodestrap/indicator'
-import { Modal, usesModalLayout, usesModalVariants, usesModalStates } from '@nodestrap/modal'
+import { Modal, usesBackdropLayout, usesBackdropVariants, usesBackdropStates } from '@nodestrap/modal'
 
 
-const usePopupLoginFormSheet = createUseSheet(() => [
+const useLoginDialogSheet = createUseSheet(() => [
     mainComposition(
         imports([
-            // import some stuff from <Modal>:
-            usesModalLayout(),
-            usesModalVariants(),
-            usesModalStates(),
+            // import some stuff from <Modal>'s dialog:
+            usesDialogLayout(),
+            usesDialogStates(),
         ]),
         style({
             // then overwrite with your style:
-            display : 'grid',
-            margin  : '1em',
+            display             : 'grid',
+            gridTemplateColumns : '1fr 1fr',
+            gridAutoFlow        : 'row',
+            gap                 : '1rem',
+            outline             : 'none',
+            
+            ...children('.input', {
+                gridColumnEnd: 'span 2',
+            }),
+            
             /* ... */
             
             ...variants([
@@ -519,25 +542,32 @@ const usePopupLoginFormSheet = createUseSheet(() => [
                 }),
                 /* ... */
             ]),
-            ...states([
-                isActive({
-                    // define the style at 'being/fully active' state:
-                    border-color: 'red',
-                    /* ... */
-                }),
-                /* ... */
-            ]),
             
             /* ... */
         }),
     ),
 ]);
 
-export default function PopupLoginForm(props) {
-    const sheet = usePopupLoginFormSheet();
+export default function LoginDialog(props) {
+    const sheet = useLoginDialogSheet();
     return (
-        <Modal {...props} mainClass={sheet.main}>
-            { props.children }
+        <Modal {...props}>
+            <Basic rule='dialog' mainClass={sheet.main}>
+                <TextInput  placeholder='John Smith'     size='sm' classes={['input]} />
+                <EmailInput placeholder='john@smith.com' size='sm' classes={['input]} />
+                <Button
+                    theme='primary'
+                    size='sm'
+                >
+                    Submit
+                </Button>
+                <Button
+                    theme='secondary'
+                    size='sm'
+                >
+                    Cancel
+                </Button>
+            </Basic>
         </Modal>
     )
 }
